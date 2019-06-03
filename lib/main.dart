c20ccd09c5a10c7d16701cc95b406d9c6e2dd26c
yuec621 <yuec621@outlook.com>
2019-06-03 15:59:13 +0800

    init hello

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+void main()=>runApp(MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'hello flutter' ,
+      home: Scaffold(
+        appBar: AppBar(title: Text('hello world'),
+        
+        ),
+        body: Center(
+          child: Text('hello---imm'),
+        ),
+      ),
+
+    );
+  }
+}
