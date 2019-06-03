329f07a916188ea30edc72c12d4e3b8455402d40
yuec621 <yuec621@outlook.com>
2019-06-03 17:13:27 +0800

    纵向列表

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ A new Flutter project.
 
 ## Getting Started
 
-#安装Android Studio ---->下载插件flutter/下载虚拟机
+#安装Android Studio ---->下载插件flutter/下载AVD虚拟机
 #安装java jdk
 #安装flutter---->环境配置
 #在vscode里面使用flutter--->
 #1.安装flutter插件 2.创建flutter create demo项目 3.打开lib/main.dart  4.打开虚拟机 5.执行flutter run
+
+#快捷键：1."r"热加载 2."o"切换ios和android 3."d"和"q"停止运行 
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(MyApp());
-class MyApp extends StatelessWidget{
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:'hello flutter' ,
+      title: 'flutter',
       home: Scaffold(
-        appBar: AppBar(title: Text('hello world'),
-        
+        appBar: AppBar(
+          title: Text('hello world'),
         ),
-        body: Center(
-          child: Text('hello---imm'),
+        body: new ListView(
+          children: <Widget>[
+            new Image.network(
+                'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
+            new Image.network(
+                'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
+            new Image.network(
+                'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
+          ],
         ),
       ),
-
     );
   }
 }
