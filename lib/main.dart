cdf4ed85e7aac74d722da9d016c6d4da38c6e6ef
yuec621 <yuec621@outlook.com>
2019-06-03 17:22:21 +0800

    横向列表

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,52 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('hello world'),
         ),
-        body: new ListView(
-          children: <Widget>[
-            new Image.network(
-                'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
-            new Image.network(
-                'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
-            new Image.network(
-                'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
-          ],
-        ),
+        body: Center(child: Container(
+          height: 200.0,
+          child: MyList(
+            
+            
+          ),
+        ),),
+
+
+
+        // body: new ListView(
+        //   children: <Widget>[
+        //     new Image.network(
+        //         'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
+        //     new Image.network(
+        //         'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
+        //     new Image.network(
+        //         'https://img1.mukewang.com/szimg/5cbf00c608f52a3b06000338.jpg'),
+        //   ],
+        // ),
       ),
+    );
+  }
+}
+
+
+class MyList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+              new Container(
+                width: 180.0,
+                color: Colors.lightBlue,
+              ),
+               new Container(
+                width: 180.0,
+                color: Colors.lightGreen,
+              ),
+               new Container(
+                width: 180.0,
+                color: Colors.limeAccent,
+              ),
+
+            ],
     );
   }
 }
